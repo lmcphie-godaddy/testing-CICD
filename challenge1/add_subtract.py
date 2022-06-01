b2acea9d8a4dbc3b8a659785fc218f3186a0d059
Leandra McPhie <lmcphie-godaddy.com>
2022-06-01 16:49:21 -0400

    reorganized files

---
+++ challenge1/add_subtract.py
@@ -0,0 +1,13 @@
+#ADD FUNCTION
+def add(a:int, b:int):
+    # print("Sum of a and b:")
+    return (a + b)
+
+#SUBTRACT FUNCTION
+def subtract(a:int, b:int):
+    # print("Subtract of a and b:")
+    return (a - b)
+
+if __name__ == '__main__':
+    print("a + b is", add(3,6))
+    print("a - b is", subtract(3,6))
