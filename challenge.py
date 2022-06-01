6c83ea4bc1569788981446b8124522a9d4fe71ef
lmcphie-godaddy <105673626+lmcphie-godaddy@users.noreply.github.com>
2022-06-01 11:30:05 -0400

    adding code

---
+++ challenge.py
@@ -0,0 +1,13 @@
+#ADD FUNCTION
+def add(a:int, b:int):
+    # print("Sum of a and b:")
+    return (a + b)
+
+#SUBTRACT FUNCTION
+def subtract(a:int, b:int):
+    # print("Subtract of a and b:")
+    return (a - b)
+
+if __name__ == '__main__':
+    print(add(3,6))
+    print(subtract(1,-4))
