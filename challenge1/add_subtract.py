dc14d68fc53cdfbaa145714193654737652cbae1
lmcphie-godaddy <lmcphie@gmail.com>
2023-07-20 15:12:26 -0400

    output format

--- challenge1/add_subtract.py
+++ challenge1/add_subtract.py
@@ -1,13 +1,13 @@
 #ADD FUNCTION
 def add(a:int, b:int):
-    # print("Sum of a and b:")
+    print("a is " + str(a) + ", b is " + str(b))
     return (a + b)
 
 #SUBTRACT FUNCTION
 def subtract(a:int, b:int):
-    # print("Subtract of a and b:")
+    print("a is " + str(a) + ", b is " + str(b))
     return (a - b)
 
 if __name__ == '__main__':
-    print("a + b is", add(3,6))
-    print("a - b is", subtract(3,6))
+    print("a + b =", add(3,6), "\n")
+    print("a - b =", subtract(3,6), "\n")
